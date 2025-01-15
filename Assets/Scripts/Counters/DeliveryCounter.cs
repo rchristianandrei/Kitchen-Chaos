4eb2864134c4e0d58940317b0a6e319b6d0b4b55
Andrei Reyes <rchristianandrei@gmail.com>
2025-01-15 22:10:44 +0800

    delivery counter game object

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject()) return;
+
+        if (!player.GetKitchenObject().TryGetPlate(out var plate)) return;
+
+        // Validate if this is plate is part of the order
+        var valid = true;
+
+        if (!valid) return;
+
+        plate.DestroySelf();
+
+        // Success!
+    }
+
+    public override void InteractAlternate(Player player)
+    {
+        
+    }
+}
